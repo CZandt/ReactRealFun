d52973529affa34c5f4da98f79d09fa48dd7546f
CZandt <cole0hardy@gmail.com>
2023-03-20 18:19:06 -0600

    Second commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to cum town</h1>
+        <h1>React fun</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
